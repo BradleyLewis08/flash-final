0e1fe633ec6e08b748a1ca07151c286362e6586b
Bradley Lewis <bradleylewis@Bradleys-MBP-2.wireless.yale.internal>
2024-02-15 15:06:48 -0500

    Update schema

--- src/main/java/org/snlab/network/PrefixTreeLoader.java
+++ src/main/java/org/snlab/network/PrefixTreeLoader.java
@@ -1,5 +1,39 @@
 package org.snlab.network;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.util.HashMap;
+
+
+
+
+class JsonTreeNode {
+	public int predicate;
+	public String leftId;
+	public String rightId;
+}
+class JsonTree {
+	public String rootId;
+	public HashMap<String, JsonTreeNode> nodes;
+}
+
 public class PrefixTreeLoader {
-	// Open tree.json file
+	public PrefixTreeLoader(String fileName) {
+		File jsonFile = new File(fileName);
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			JsonTree tree = mapper.readValue(jsonFile, JsonTree.class);
+			JsonTreeNode rootNode = tree.nodes.get(tree.rootId);
+			if(rootNode == null) {
+				throw new Exception("Root node not found");
+			}
+
+			TreeNode root = new TreeNode()
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
